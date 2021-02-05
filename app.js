a7426d42a109e04360a8012881326f87e730ab35
Nick Gersbach <nicholas.j.gersbach@gmail.com>
2021-02-04 17:08:45 -0700

    examine dom

---
+++ app.js
@@ -0,0 +1,36 @@
+let val;
+
+
+//View all elements on the web page in the console
+val = document;
+console.log(val);
+
+//Access parts of the document using 0 based index
+val = document.all[5];
+console.log(val);
+
+//Grab specific parts of document by name
+val = document.head;
+console.log(val);
+
+val = document.body;
+console.log(val);
+
+val = document.doctype;
+console.log(val);
+
+val = document.domain;
+console.log(val);
+
+val = document.URL;
+console.log(val);
+
+val = document.characterSet;
+console.log(val);
+
+val = document.contentType;
+console.log(val);
+
+//Get forms
+val = document.forms;
+console.log(val);
